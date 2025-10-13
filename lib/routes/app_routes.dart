928e62f2fe5912cffdef0f22651fc2d4e0109fed
williamthegreatsaviour <114980185+williamthegreatsaviour@users.noreply.github.com>
2025-10-13 13:47:23 -0700

    Create app_routes.dart

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../screens/splash_screen.dart';
+import '../screens/login_screen.dart';
+import '../screens/home_screen.dart';
+import '../screens/movie_detail_screen.dart';
+import '../screens/profile_screen.dart';
+import '../screens/error_screen.dart';
+
+final Map<String, WidgetBuilder> appRoutes = {
+  '/splash': (context) => const SplashScreen(),
+  '/login': (context) => const LoginScreen(),
+  '/home': (context) => const HomeScreen(),
+  '/movie-detail': (context) => const MovieDetailScreen(),
+  '/profile': (context) => const ProfileScreen(),
+  '/error': (context) => const ErrorScreen(),
+};
