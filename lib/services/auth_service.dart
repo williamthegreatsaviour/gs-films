b39f25e36ca272ac74d7e3f67fb060cb6dabbb21
williamthegreatsaviour <114980185+williamthegreatsaviour@users.noreply.github.com>
2025-10-13 13:50:12 -0700

    Create auth_service.dart

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../utils/constants.dart';
+
+class AuthService {
+  Future<Map<String, dynamic>> login(String email, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/login'),
+      body: {'email': email, 'password': password},
+    );
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Error al iniciar sesión');
+    }
+  }
+}
