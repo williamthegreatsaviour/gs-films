babc818c6d2fb41f3b188ec742d924252a614ae4
williamthegreatsaviour <114980185+williamthegreatsaviour@users.noreply.github.com>
2025-10-13 13:45:45 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
-import 'package:cinepulso/theme.dart';
-import 'package:cinepulso/providers/auth_provider.dart';
-import 'package:cinepulso/providers/movie_provider.dart';
-import 'package:cinepulso/screens/splash_screen.dart';
+import 'routes/app_routes.dart';
+import 'screens/splash_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  
-  // Set system UI overlay style
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.light,
-      systemNavigationBarColor: GSFilmsColors.black,
-      systemNavigationBarIconBrightness: Brightness.light,
-    ),
-  );
-  
+void main() {
   runApp(const GSFilmsApp());
 }
 
@@ -27,17 +11,13 @@ class GSFilmsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
-        ChangeNotifierProvider(create: (_) => MovieProvider()),
-      ],
-      child: MaterialApp(
-        title: 'GSFilms - Tu portal al cine',
-        debugShowCheckedModeBanner: false,
-        theme: darkTheme, // Always use dark theme as per design requirements
-        home: const SplashScreen(),
-      ),
+    return MaterialApp(
+      title: 'GSFilms',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(useMaterial3: true),
+      initialRoute: '/splash',
+      routes: appRoutes,
     );
   }
 }
+
