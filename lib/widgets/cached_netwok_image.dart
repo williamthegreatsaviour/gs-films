ce751e5bcfcd7fb6ef7ea0bac1c4eed530f94eab
williamthegreatsaviour <114980185+williamthegreatsaviour@users.noreply.github.com>
2025-10-31 15:49:47 -0700

    Update cached_netwok_image.dart

--- lib/widgets/cached_netwok_image.dart
+++ lib/widgets/cached_netwok_image.dart
@@ -1,7 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
-cachedImageNetwort(String imageURL) => CachedNetworkImage(
+Widget cachedImageNetwork(String imageURL) => CachedNetworkImage(
       imageUrl: imageURL,
       fit: BoxFit.cover,
       placeholder: (context, url) {
